e2c4f2d30865875fedc070214bed2bfcbfc02a1e
Александр Солодовников <A.Solodovnikov.PGES@yandex.ru>
2022-05-22 10:15:21 +0300

    creating applications: post, blog

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'post.apps.PostConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
